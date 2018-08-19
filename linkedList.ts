d6d9e49bfd65b8f108e7b78f88da8c87693234e1
Maor <Offir.Maor@verint.com>
2018-08-19 20:28:28 +0300

    rrr

--- linkedList.ts
+++ linkedList.ts
@@ -1,8 +1,4 @@
 class LinkedList
 {
-    public name:string="ffffdasdfsasfaffgfgfdfgfff";
-    public name1:string="ffffdasdfsasfaffgfgfdfgfff";
-   
-    public namsse1:string="ffffdasdfsasfaffgfgfdfgfff";
-    public namssde1:string="ffffdasdfsasfaffgfgfdfgfff";
+    
 }
