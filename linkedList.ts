2c971c40827776aa02a8e68ee9fa2f0a276a77d1
Maor <Offir.Maor@verint.com>
2018-08-19 20:42:47 +0300

    fffdf

--- linkedList.ts
+++ linkedList.ts
@@ -1,4 +1,7 @@
 class LinkedList
 {
-    
+    public name:string = "ffffdasdfsasfaffgfgfdfgfff";
+    public name1:string = "ffffdasdfsasfaffgfgfdfgfff";
+    public namsse1:string = "ffffdasdfsasfaffgfgfdfgfff";
+    public namssdxvffgggxczve1:string = "ff";
 }
