b61b2fe3f61967a6c92da47e67e282b48213db43
Maor, Offir <Offir.Maor@verint.com>
2018-08-30 10:55:41 +0300

    never mind

--- linkedList.ts
+++
@@ -1,7 +0,0 @@
-class LinkedList
-{
-    public name:string = "ffffdasdfsasfaffgfgfdfgfff";
-    public name1:string = "ffffdasdfsasfaffgfgfdfgfff";
-    public namsse1:string = "ffffdasdfsasfaffgfgfdfgfff";
-    public namssdxvffgggxczve1:string = "ff";
-}
