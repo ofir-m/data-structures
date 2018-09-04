29917f5e31e7f6024b5301ece114e46726a399e0
Maor, Offir <Offir.Maor@verint.com>
2018-09-04 15:04:20 +0300

    ff

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use:['style-loader','css-loader', 'sass-loader']
       }
     ]
   },
